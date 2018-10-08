543851c3e750fc65787a87d7ab40031e64c2bbfd
satoto prayudi <satoto.prayudi@gmail.com>
2018-10-09 05:24:26 +0700

    Generate data service untuk mengambil data dari API metaweather

---
+++ src/app/data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class DataService {
+
+	/** By pass CORS issue */
+	CORSBYPASS = 'https://cors-anywhere.herokuapp.com/';
+
+	/** API HOST */
+	API_HOST = this.CORSBYPASS+'https://www.metaweather.com/';
+
+	constructor(
+		private http : HttpClient
+	) {}
+
+	getWoeId(latitude:number, longitude:number){
+		return new Promise((resolve, error) => {
+			this.http.get(this.API_HOST+'api/location/search/?lattlong='+latitude+','+longitude)
+			.subscribe(result => {
+				resolve(result);
+			}, (errorResponse : HttpErrorResponse) => {
+				error(errorResponse);
+			})
+		})
+	}
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	lat : number;
+	long : number;
+	woeid : number;
 
-  ngOnInit() {
-  }
+	constructor(
+		private dataService : DataService
+	) { }
 
+	ngOnInit() {
+		/* Ambil latitude & longitude posisi saat ini */
+		this.getCurrentLocation();
+	}
+
+	getCurrentLocation(){
+		if(navigator.geolocation){
+			navigator.geolocation.getCurrentPosition((position) => {
+				this.lat = position.coords.latitude;
+				this.long = position.coords.longitude;
+				this.getWoeId();
+			});
+		} else {
+			/* Jika browser tidak support, set default latitude & longitude Jakarta */
+			this.lat = -6.171440;
+			this.long = 106.827820;
+		}
+	}
+
+	getWoeId(){
+		this.dataService.getWoeId(this.lat,this.long)
+		.then(result => {
+			console.log(result);
+		});
+	}
 }
